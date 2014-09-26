3a3639c0c37c41045e875598fb90e342e21a67b8
Graeme Wolfendale <gwolfendale@gmail.com>
2014-09-26 06:58:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-var pagediv = document.getElementById("div0");
-pagediv.innerHTML = "<strong>text added with javascript</strong>"
+var pagediv = document.getElementById("div0"); //here we make a pointer to the div we created in html
+pagediv.innerHTML = "<strong>text added with javascript</strong>" //and put some text inside it
